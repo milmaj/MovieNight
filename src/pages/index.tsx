3b79fac5670ae69c2dd7a3784b50512539d47f14
milmaj <->
2020-09-30 01:45:30 +0200

    resolves  #5 - added test data from themoviedb.org

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import { data } from '@/example';
+import { Card, Col, Row, Avatar } from 'antd';
+import { PlaySquareOutlined, InfoOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
+
 export default () => {
     return (
         <div data-page="index">
-            <h1>Page index</h1>
+            <Row gutter={[16, 16]}>
+                {data.results.map((i, index) => (
+                    <Col span={8} key={index}>
+                        <Card
+                            cover={<img alt="example" src={`https://image.tmdb.org/t/p/w500/${i.backdrop_path}`} />}
+                            actions={[<PlaySquareOutlined key="play" />, <InfoOutlined key="info" />]}
+                        >
+                            <Meta
+                                avatar={<Avatar src={`https://image.tmdb.org/t/p/w500/${i.poster_path}`} />}
+                                title={i.title}
+                                description={i.overview}
+                            />
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         </div>
     );
 };
